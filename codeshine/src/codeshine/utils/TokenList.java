95a3b5b6cd54f23da99d57eab0525e2f87b43419
albelmelg <albelmel@inf.upv.es>
2017-06-09 17:49:09 +0200

    Se añaden los comentarios de tipo Javadoc

--- codeshine/src/codeshine/utils/TokenList.java
+++ codeshine/src/codeshine/utils/TokenList.java
@@ -6,15 +6,17 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.List;
+
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
+import org.apache.log4j.Logger;
 import org.xml.sax.helpers.DefaultHandler;
 
 import codeshine.preferences.IPreferenceConstants;
 
 public class TokenList extends DefaultHandler{
-	
+	private static Logger logger = Logger.getLogger(TokenList.class);
 	String[][] configData = {{"/**", "Javadoc string", "Cadena de documentación"},
 		{"//", "single line comment","Comentario a una linea"},
 			{"/*", "multiline comment", "Comentario de varias lineas"},
@@ -22,7 +24,10 @@ public class TokenList extends DefaultHandler{
 
 	final List list = new ArrayList();
 	final Hashtable table = new Hashtable();
-	
+	/**
+	 * Crea una lista tokenlist
+	 * @param argv Los argumentos que recibe el método
+	 */
 	public static void main(String[] argv){
 			TokenList tl = null;
 			try{
@@ -30,11 +35,12 @@ public class TokenList extends DefaultHandler{
 			}catch (InvalidObjectException e){e.printStackTrace();}
 		
 	}
+	/**
+	 * Contructor de listas de tipo Token
+	 */
 
 	public TokenList() {
 		for (int i=0; i<configData.length; i++){
-
-			
 			Token t = new Token(configData[i][0],
 									configData[i][1],
 									configData[i][2]);
@@ -43,9 +49,11 @@ public class TokenList extends DefaultHandler{
 		}
 	}
 	/**
+	 *  Analiza el contenido del archivo especificado fInput
+	 * como XML utilizando el archivo org.xml.sax.helpers.DefaultHandler.
 	 * 
-	 * 	 
-	 * @param configFile
+	 * @param fInput Un objeto de tipo File
+	 * @throws InvalidObjectException
 	 */
 	public TokenList(File fInput) throws InvalidObjectException{
 		DefaultHandler handler = new XMLHandler(this);
@@ -60,45 +68,71 @@ public class TokenList extends DefaultHandler{
         }catch (Throwable t) {}
         this.addToken(new Token(IPreferenceConstants.SPECIAL,"...","..."));
 	}
+	/**
+	 * Crea un objeto de tipo File dado un pathfile
+	 * @param configFile el pathfile
+	 * @throws InvalidObjectException
+	 */
+	
 	public TokenList(String configFile) throws InvalidObjectException{
 		this(new File(configFile));
 	}
-	
+	/**
+	 * Devuelve un array de objetos
+	 * @return Un array de objetos en el mismo orden en el que estaba
+	 */
 	public Object[] toArray(){
 
 		return this.table.values().toArray();
 	}
-
+	/**
+	 * Añade un token a la lista
+	 * @param t el token que se añade a la lista
+	 */
 	public void addToken(Token t){
-		System.out.println("#elementos: " + this.length());
+		logger.info("#elementos: " + this.length());
 		if (table.containsKey(t.getValue())){
-			System.out.println("Actualizando elemento existente");
-
+			logger.info("Actualizando elemento existente");
 			table.put(t.getValue(), t);
-
 		}
 		else{
-			System.out.println("Añadido nuevo elemento");
+			logger.info("Añadido nuevo elemento");
 			table.put(t.getValue(),t);
 		}
-		System.out.println("NumElementos: " + this.table.size());
+		logger.info("NumElementos: " + this.table.size());
 	}
+	/**
+	 * Elimina el token que se especifica en el String key
+	 * @param key Especifica el token a eliminar
+	 */
 	public void removeToken(String key){
 		if (!key.equals(IPreferenceConstants.SPECIAL))
 			this.table.remove(key);
 	}
+	/**
+	 * Devuelve un string literal del objeto
+	 */
 	public String toString(){
 		String value = "";
-
 		Enumeration elems = table.elements();
 		while (elems.hasMoreElements())
 			value += elems.nextElement().toString();
 		return value;
 	}
+	/**
+	 * Devuelve la longitud de la TokenList
+	 * @return Un int que indica la longitud de la TokenList
+	 */
 	public int length(){
 		return table.size();
 
 	}
+	/**
+	 * Convierte el String que se le pasa a formato XML
+	 * @param filepath el string que queremos convertir
+	 * @return Un String en formato XML
+	 */
+	
 	public String toXML(String filepath){
 		this.table.remove(IPreferenceConstants.SPECIAL);
 		XMLWriter writer = null;
@@ -108,21 +142,35 @@ public class TokenList extends DefaultHandler{
 		writer.writeFile();
 		return null;
 	}
+	/**
+	 * 
+	 * @return
+	 */
 	public Trie toTrie(){
 		Trie ret = new Trie();
-
 		Enumeration elems = table.elements();
 		while (elems.hasMoreElements())
 			ret.addToken(elems.nextElement());
-
 		return ret;
 	}
+	/**
+	 * Indica el numero de elementos en un Hashtable
+	 * @return Devuelve un int con el numero de elementos en el hastable
+	 */
 	public Enumeration getElements(){
 		return this.table.elements();	
 	}
+	/**
+	 * Devuelve una coleccion de objetos de tipo HashTable
+	 * @return La coleccion de objetos Hashatble
+	 */
 	public Hashtable getCollection(){
 		return this.table;
 	}
+	/**
+	 * Establece si los objetos Key del Hashtable son iguales
+	 * @return True si los objetos Key del Hashtable son iguales o False en sentido contrario
+	 */
 	public boolean equals(Object obj){
 		boolean ret = true;
 		try{
