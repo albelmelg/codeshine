26aaa6fcd475eab2db99c403e92616990dccdb5f
albelmelg <albelmel@inf.upv.es>
2017-06-09 13:05:22 +0200

    Hacemos el javadoc de esta clase

--- codeshine/src/codeshine/utils/Token.java
+++ codeshine/src/codeshine/utils/Token.java
@@ -5,38 +5,79 @@ public class Token {
 	private String replacement;
 	private String info;
 
-	
+/**
+ * Constructor vacio
+ */
 	public Token(){
 		this.value = "";
 		this.replacement = "";
 		this.info = "";
 	}
+/**
+ * Constructor de objeto token
+ * @param value
+ * @param replacement
+ * @param info
+ */
 	public Token(String value, String replacement, String info){
 		this.value = value;
 		this.replacement = replacement;
 		this.info = info;
 	}
+	
+	/**
+	 * Construye una cadena de String con los datos del objeto
+	 */
 	public String toString(){
 		return (this.value + "  " + this.replacement + "  " + this.info + "\n");
 	}
+	/**
+	 * Devuelve el valor que hay en Value
+	 * @return Un string
+	 */
 	public String getValue(){
 		return this.value;
 	}
+	/**
+	 * Devuelve el valor que hay en Replacement
+	 * @return Un string
+	 */
 	public String getReplacement(){
 		return this.replacement;
 	}
+	/**
+	 * Devuelve el valor que hay en info
+	 * @return Un string
+	 */
 	public String getInfo(){
 		return this.info;
 	}
+	/**
+	 * Establece un nuevo valor para value
+	 * @param value un string del objeto Token
+	 */
 	public void setValue(String value){
 		this.value = value;
 	}
+	/**
+	 * Establece un nuevo valor para info
+	 * @param info un string del objeto Token
+	 */
 	public void setInfo(String info){
 		this.info = info;
 	}
+	/**
+	 * Establece un nuevo valor para replacement
+	 * @param replacement un string del objeto Token
+	 */
 	public void setReplacement(String replacement){
 		this.replacement = replacement;
 	}
+	/**
+	 * Devuelve True o False si los parametros value de dos objetos token son iguales
+	 * @param token el objeto token
+	 * @return True si son iguales, False si no los son
+	 */
 	public boolean equals(Token token){
 		return this.value.equals(token.getValue());
 	}
